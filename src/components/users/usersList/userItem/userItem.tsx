9ede2b6553369433f9f2debc620e4246e09e9e3b
bruno.afonso <bruno.afonso94@hotmail.com>
2025-06-20 16:42:50 +0100

    Feedback on user delete

--- src/components/users/usersList/userItem/userItem.tsx
+++ src/components/users/usersList/userItem/userItem.tsx
@@ -6,18 +6,27 @@ import { memo, useCallback } from "react";
 import * as S from "./userItem.styles";
 import { Paragraph } from "@/components/globals/fonts";
 import { Trash } from "lucide-react";
+import { useAlertStore } from "@/hooks/useAlertStore";
+import { AlertStore } from "@/store/alertStore";
+import { ErrorI } from "@/types/api";
 
 interface Props {
   user: User;
 }
 
 const UserItem = ({ user }: Props) => {
+  const updateAlert = useAlertStore((state: AlertStore) => state.updateAlert);
+
   const handleDelete = useCallback(async () => {
     try {
-      const response = await UsersService.deleteUser(user.id);
-      return response;
-    } catch {}
-  }, [user]);
+      await UsersService.deleteUser(user.id);
+
+      updateAlert({ message: "User deleted", status: 200 });
+    } catch (error) {
+      const e = error as ErrorI;
+      updateAlert({ message: e.message, status: e.status });
+    }
+  }, [user, updateAlert]);
 
   return (
     <S.UserItem>
